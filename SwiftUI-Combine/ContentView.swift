72cc04b105bfcd2160f8065d03eb93cb1e4d5f1e
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-10-25 21:22:45 +0500

    Initial Commit

---
+++ SwiftUI-Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine
+//
+//  Created by Arkasha Zuev on 25.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Combine/SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineApp.swift
+//  SwiftUI-Combine
+//
+//  Created by Arkasha Zuev on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
